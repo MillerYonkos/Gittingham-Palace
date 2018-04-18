61a5cfc4b7a9e98b40d73b73893e1f51abee796a
Miller Yonkos <my134424@g.risd.org>
2018-04-18 16:10:30 -0500

    Added scanner and input msg

--- TheBeginning.java
+++ TheBeginning.java
@@ -1,2 +1,12 @@
 //first line of the pronhub chronicles!
+import java.util.*;
 
+public class TheBeginning
+{
+	public static void main(String[] args) throws IOExceptions
+	{
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter Your Message to be Hashed");
+	}
+}
