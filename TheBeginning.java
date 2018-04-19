b09f4765ae18b78c9fa09ae404979f6e17899603
Miller Yonkos <my134424@g.risd.org>
2018-04-19 16:10:40 -0500

    added method

--- TheBeginning.java
+++ TheBeginning.java
@@ -1,12 +1,22 @@
 //first line of the pronhub chronicles!
 import java.util.*;
-
+import static java.lang.System.*;
 public class TheBeginning
 {
-	public static void main(String[] args) throws IOExceptions
+	public static void main(String[] args)
 	{
 		Scanner input = new Scanner(System.in);
-cream
-		System.out.println("Enter Your Message to be Hashed");
+		out.println("Enter Your Message to be Hashed");
+		String message = input.next();
+
+	}
+	public static int hashCode(String jonny){
+		int total = 0;
+		for(int i = 0; i <= jonny.length(); i++){
+			int nam = jonny.charAt(i);
+			total += nam;
+		}
+
+		return total;
 	}
 }
